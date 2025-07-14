ac7c34f195e70058292f9543e1eca451f4afd822
Victor-Jose Lande <36843312+VICTORJOSEARAVENALANDE@users.noreply.github.com>
2025-07-14 16:53:59 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env bash
 set -e
 echo "==================="
-# Git setup
 git config --global user.name  "${INPUT_NAME}"
 git config --global user.email "${INPUT_EMAIL}"
 git config --global --add safe.directory /github/workspace
 
-# Run generator
-python3 /github/workspace/feed.py
+python3 /usr/bin/feed.py
 
-# Commit & push only if portfolio.xml changed
 if ! git diff --quiet -- portfolio.xml; then
   git add portfolio.xml
   git commit -m "chore: update portfolio feed"
